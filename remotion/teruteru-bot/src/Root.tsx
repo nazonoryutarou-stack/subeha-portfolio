8b3e0d9c713531c8d9d5ec66292df724890003c2
nazonoryutarou-stack <nazonoryutarou@gmail.com>
2026-08-16 20:25:07 +0900

    Switch to dense exoskeleton Teruteru Bot CG

--- remotion/teruteru-bot/src/Root.tsx
+++ remotion/teruteru-bot/src/Root.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Composition} from 'remotion';
-import {TeruteruBotCG} from './TeruteruBotCG';
+import {TeruteruBotCG2} from './TeruteruBotCG2';
 
 export const Root: React.FC = () => (
   <Composition
     id="TeruteruBotCG"
-    component={TeruteruBotCG}
+    component={TeruteruBotCG2}
     durationInFrames={450}
     fps={30}
     width={720}
