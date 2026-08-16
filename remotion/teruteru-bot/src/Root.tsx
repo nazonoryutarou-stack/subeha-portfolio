37374b8e609d52f7e7c58b514779ed235bff4f67
nazonoryutarou-stack <nazonoryutarou@gmail.com>
2026-08-16 19:53:34 +0900

    Use 15 second review composition

--- remotion/teruteru-bot/src/Root.tsx
+++ remotion/teruteru-bot/src/Root.tsx
@@ -6,7 +6,7 @@ export const Root: React.FC = () => (
   <Composition
     id="TeruteruBotCM"
     component={TeruteruBotCM}
-    durationInFrames={900}
+    durationInFrames={450}
     fps={30}
     width={1080}
     height={1920}
