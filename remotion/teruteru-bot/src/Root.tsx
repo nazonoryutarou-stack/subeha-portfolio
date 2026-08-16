9e4d6dd44a6b1d92cdbc4927e7425fa1fc850776
nazonoryutarou-stack <nazonoryutarou@gmail.com>
2026-08-16 19:39:28 +0900

    Expand Teruteru Bot CM to 30 seconds

--- remotion/teruteru-bot/src/Root.tsx
+++ remotion/teruteru-bot/src/Root.tsx
@@ -6,7 +6,7 @@ export const Root: React.FC = () => (
   <Composition
     id="TeruteruBotCM"
     component={TeruteruBotCM}
-    durationInFrames={450}
+    durationInFrames={900}
     fps={30}
     width={1080}
     height={1920}
