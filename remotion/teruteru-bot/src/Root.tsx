60833d7dbea374205c1e458053d7eda6c69274fc
nazonoryutarou-stack <nazonoryutarou@gmail.com>
2026-08-16 20:45:24 +0900

    Use story-driven Teruteru Bot CM

--- remotion/teruteru-bot/src/Root.tsx
+++ remotion/teruteru-bot/src/Root.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Composition} from 'remotion';
-import {TeruteruBotCG2} from './TeruteruBotCG2';
+import {TeruteruBotScenario} from './TeruteruBotScenario';
 
 export const Root: React.FC = () => (
   <Composition
     id="TeruteruBotCG"
-    component={TeruteruBotCG2}
+    component={TeruteruBotScenario}
     durationInFrames={450}
     fps={30}
     width={720}
