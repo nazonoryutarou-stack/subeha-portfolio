23061d21297e3ac54f613274bd3db2970e4c07db
nazonoryutarou-stack <nazonoryutarou@gmail.com>
2026-08-22 03:49:45 +0900

    Enable HOST voice enrollment in Studio

--- tools/vrm-talker/src/main.js
+++ tools/vrm-talker/src/main.js
@@ -2,5 +2,6 @@ import './main-studio.js';
 import './studio-panel.js';
 import './api-settings.js';
 import './vrm-cache.js';
+import './speaker-reference.js';
 import './visual-director.js';
 import './visual-overlay.js';
