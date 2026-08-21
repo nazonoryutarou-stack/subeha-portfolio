fb01562ce999279f4a597c92234cb40bd91dbc43
nazonoryutarou-stack <nazonoryutarou@gmail.com>
2026-08-22 03:52:56 +0900

    Show transcription progress in Studio

--- tools/vrm-talker/src/main.js
+++ tools/vrm-talker/src/main.js
@@ -3,5 +3,6 @@ import './studio-panel.js';
 import './api-settings.js';
 import './vrm-cache.js';
 import './speaker-reference.js';
+import './transcription-progress.js';
 import './visual-director.js';
 import './visual-overlay.js';
