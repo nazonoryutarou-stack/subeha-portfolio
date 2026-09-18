e5b204ac63673e757d38bfece17d76735c73cf77
nazonoryutarou-stack <nazonoryutarou@gmail.com>
2026-09-18 23:07:41 +0900

    Register VTuber backup composition

--- remotion/vrm-lipsync/src/Root.tsx
+++ remotion/vrm-lipsync/src/Root.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import {Composition} from 'remotion';
 import {VrmLipSync} from './VrmLipSync';
+import {VrmLipSyncBackup} from './backup/VrmLipSyncBackup';
+import {backupMeta} from './backup/generatedTimeline';
+
+const FPS=30;
 
 export const Root: React.FC = () => (
-  <Composition
-    id="VrmLipSync"
-    component={VrmLipSync}
-    durationInFrames={900}
-    fps={30}
-    width={720}
-    height={1280}
-  />
+  <>
+    <Composition
+      id="VrmLipSync"
+      component={VrmLipSync}
+      durationInFrames={900}
+      fps={FPS}
+      width={720}
+      height={1280}
+    />
+    <Composition
+      id="VrmLipSyncBackup"
+      component={VrmLipSyncBackup}
+      durationInFrames={Math.max(1,Math.ceil(backupMeta.durationMs/1000*FPS))}
+      fps={FPS}
+      width={720}
+      height={1280}
+    />
+  </>
 );
